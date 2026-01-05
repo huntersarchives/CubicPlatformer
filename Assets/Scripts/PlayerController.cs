5c2736ec2fc1bcee413f76dcfb2740f7da06e909
Hunter <hunterweisenbach@me.com>
2026-01-04 23:19:43 -0500

    commented out the debug log

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,7 +25,8 @@ public class PlayerController : MonoBehaviour
         // set movement from input manager
         movement.Set(InputManager.movement.x, InputManager.movement.y);
         JumpLogic();
-        Debug.Log(extraJump);
+
+        //Debug.Log(extraJump);
     }
 
     void FixedUpdate()
