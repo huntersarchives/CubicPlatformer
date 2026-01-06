fe95f7f0d058f9e3292b06230a01d532d86bf5e5
Hunter <hunterweisenbach@me.com>
2026-01-06 16:48:47 -0500

    moving to windoes pc

--- Assets/Scripts/DoubleJumpCollectable.cs
+++ Assets/Scripts/DoubleJumpCollectable.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class DoubleJumpCollectable : MonoBehaviour
 {
     [SerializeField] public ParticleSystem ps;
+    public SpriteRenderer sr;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        PlayerController player = collision.GetComponent<PlayerController>();
         if (collision.tag == "Player")
         {
             // Gives the player a bonus jump
-            // **TODO** particle system when picked up and outline sprite when character has bonusjump available
-            PlayerController player = collision.GetComponent<PlayerController>();
+            // **TODO** particle system when picked up and outline sprite when character has bonusjump availabl
             player.bonusJump += 1;
             ps.Play();
             Destroy(gameObject);
