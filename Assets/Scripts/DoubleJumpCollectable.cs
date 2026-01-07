f6dcddb726b53da49d65a2710253c792642cbc4c
Hunter <hunterweisenbach@me.com>
2026-01-06 22:46:29 -0500

    doublejumpUI

--- Assets/Scripts/DoubleJumpCollectable.cs
+++ Assets/Scripts/DoubleJumpCollectable.cs
@@ -1,16 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class DoubleJumpCollectable : MonoBehaviour
 {
     [SerializeField] public ParticleSystem ps;
-    public SpriteRenderer sr;
+    public TextMeshProUGUI bonusJumpText;
+    public PlayerController player;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerController player = collision.GetComponent<PlayerController>();
         if (collision.tag == "Player")
         {
             // Gives the player a bonus jump
-            // **TODO** particle system when picked up and outline sprite when character has bonusjump availabl
+            // **TODO** particle system when picked up and outline sprite when character has bonusjump available
+            
             player.bonusJump += 1;
             ps.Play();
             Destroy(gameObject);
---
+++ Assets/Scripts/JumpUI.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class JumpUI : MonoBehaviour
+{
+    public PlayerController player;
+    public TextMeshProUGUI bonusJumpText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.bonusJump > 0)
+        {
+            bonusJumpText.text = "Bonus Jump Available";
+        }
+        if (player.bonusJump == 0)
+        {
+            bonusJumpText.text = "No Bonus Jump Available!";
+        }
+
+    }
+}
+
