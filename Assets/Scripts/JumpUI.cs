f67b0a0f95879a208b75bc38edc239863c15372e
Hunter <hunterweisenbach@me.com>
2026-01-10 20:33:51 -0500

    Update JumpUI.cs

--- Assets/Scripts/JumpUI.cs
+++ Assets/Scripts/JumpUI.cs
@@ -6,7 +6,8 @@ public class JumpUI : MonoBehaviour
     public PlayerController player;
     public TextMeshProUGUI bonusJumpText;
 
-    // Update is called once per frame
+    // Update is called once per fram
+    // bonusjump
     void Update()
     {
         if (player.bonusJump > 0)
